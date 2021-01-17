a84c924ffd4032a4a878d6c2168c15cac94bf2c2
Nieman <lukasz_golojuch@o2.pl>
2021-01-17 19:08:34 +0100

    boczne menu
    dodane przyciski

--- apka/ekran_boczne_menu.h
+++ apka/ekran_boczne_menu.h
@@ -9,14 +9,24 @@
 #include <FMX.Colors.hpp>
 #include <FMX.Objects.hpp>
 #include <FMX.Types.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
 //---------------------------------------------------------------------------
 class TForma_boczne_menu : public TForm
 {
 __published:	// IDE-managed Components
-	TColorBox *ColorBox1;
+	TColorBox *ColorBox_biel;
 	TImage *Image_EB;
-	TImage *Image1;
-	TImage *Image2;
+	TImage *Image_arrow;
+	TImage *Image_dom;
+	TButton *Btn_rejestracja;
+	TButton *Btn_przedmioty;
+	TButton *Btn_logowanie;
+	TButton *Btn_wyloguj;
+	TButton *Btn_admin;
+	TButton *Btn_profil;
+	TButton *Btn_koszyk;
+	TColorBox *ColorBox4;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForma_boczne_menu(TComponent* Owner);
