e6a469239946c475a976e1be377e29928dc0a6ce
Patryk <pgrabowski2012@gmail.com>
2021-01-20 13:58:51 +0100

    dodanie bazy

--- apka/ekran_ebazarek.h
+++ apka/ekran_ebazarek.h
@@ -20,6 +20,8 @@
 #include <FMX.Objects.hpp>
 
 #include "../class_library/class_library.h"
+#include <Data.DB.hpp>
+#include <Data.Win.ADODB.hpp>
 //---------------------------------------------------------------------------
 class TForma_ekran_bazarek : public TForm
 {
@@ -33,6 +35,7 @@ __published:	// IDE-managed Components
 	TCurrencyColumn *grid_colKwota;
 	TImage *Image1;
 	TImage *Image2;
+	TADOConnection *ADOConnection;
 	void __fastcall btn_szukajClick(TObject *Sender);
 	void __fastcall grid_colNazwaTap(TObject *Sender, const TPointF &Point);
 	void __fastcall Image1Click(TObject *Sender);
